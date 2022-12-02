079b2d053b5e0b614e241dbc6f8cb7e353a826e2
Josh Narvaez <jhnarva@gmail.com>
2022-12-02 00:22:03 -0600

    properly fetching data, render in browser on cards next

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,12 +1,26 @@
 import {useState, useEffect } from "react";
 import { Box, Stack, Typography } from "@mui/material";
+
+import { fetchFromAPI } from "../utils/fetchFromApi";
 import { Sidebar, Videos } from "./";
 
 const Feed = () => {
+
+const [ selectedCategory, setSelectedCategory ] = useState('New');
+const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+    .then((data) => setVideos(data.items))
+  }, [selectedCategory]);
+
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
       <Box sx={{ height: { sx: 'auto', md: '92vh' }, borderRight: '1px solid #3d3d3d', px: {sx: 0, md: 2 }}}>
-        <Sidebar/>
+        <Sidebar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
         
         <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: '#fff' }}>
           Copyright 2022 ViewTube Media
@@ -14,9 +28,9 @@ const Feed = () => {
       </Box>
       <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2}}>
         <Typography variant="h4" fontWeight="bold" mb={2} sx={{color: 'white' }}>
-            New <span style={{ color: '#8789ff'}}>videos</span>
+            {selectedCategory} <span style={{ color: '#8789ff'}}>videos</span>
           </Typography>
-          <Videos videos={[]} />
+          <Videos videos={videos} />
       </Box>
     </Stack>
   )
